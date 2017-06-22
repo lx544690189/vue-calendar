1747937772326fcb2f0817a5dd56d28228baceeb
lx544690189 <lx544690189@users.noreply.github.com>
2017-06-22 23:42:02 +0800

    增加demo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
 	<div id="app">
-		<button @click="initCalendar">打开calendar</button>
+		<p>DEMO1：默认已选日期为当天</p>
+		<p class="detail">
+			当前日期：{{ demo1.selectedDate }}
+		</p>
+		<button @click="demo1Click">打开/关闭日期选择器</button>
 		<calendar
-			v-model="calendarShow"
-			format="yyyy-MM-dd"
-			:defaultDate="defaultDate"
-			@onChange="dateChange">
+			v-model="demo1.calendarShow"
+			:defaultDate="demo1.defaultDate"
+			@onChange="demo1DateChange">
 		</calendar>
-		<div>
-			{{ selectedDate }}
-		</div>
+		<p>DEMO2：多语言-英语</p>
+		<button @click="demo2Click">打开/关闭日期选择器</button>
+		<calendar
+			v-model="demo2.calendarShow"
+			:month="demo2.month"
+			:week="demo2.week">
+		</calendar>
+		<p>DEMO3：日期格式化</p>
+		<p class="detail">
+			当前日期：{{ demo3.selectedDate }}
+		</p>
+		<button @click="demo3Click">打开/关闭日期选择器</button>
+		<calendar
+			v-model="demo3.calendarShow"
+			:format="demo3.format"
+			@onChange="demo3DateChange">
+		</calendar>
 	</div>
 </template>
 
@@ -18,19 +35,62 @@
 		name: 'app',
 		data() {
 			return {
-				calendarShow: false,
-				defaultDate:new Date(),
-				selectedDate:""
+				demo1 :{
+					calendarShow: false,
+					defaultDate:new Date(),
+					selectedDate:dateFormat(new Date(),"yyyy-MM-dd")
+				},
+				demo2 :{
+					calendarShow: false,
+					month:["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+					week:["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+				},
+				demo3 :{
+					calendarShow: false,
+					format:"yyyy年MM月dd日",
+					selectedDate:""
+				},
 			}
 		},
 		methods: {
-			initCalendar() {
-				this.calendarShow = !this.calendarShow;
+			demo1Click() {
+				this.demo1.calendarShow = !this.demo1.calendarShow;
 			},
-			dateChange(date,formatDate){
-				this.selectedDate = formatDate;
-				console.log(date)
+			demo2Click() {
+				this.demo2.calendarShow = !this.demo2.calendarShow;
+			},
+			demo3Click() {
+				this.demo3.calendarShow = !this.demo3.calendarShow;
+			},
+			demo1DateChange(date,formatDate){
+				this.demo1.selectedDate = formatDate;
+			},
+			demo3DateChange(date,formatDate){
+				this.demo3.selectedDate = formatDate;
 			}
 		}
 	}
+	
+	function dateFormat(date,fmt) {
+		var o = {
+			"M+": date.getMonth() + 1,
+			"d+": date.getDate()
+		};
+		if(/(y+)/.test(fmt))
+			fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+		for(var k in o)
+			if(new RegExp("(" + k + ")").test(fmt))
+				fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+		return fmt;
+	}
 </script>
+
+<style type="text/css">
+	button{
+		width: 100%;
+		padding: 5px;
+	}
+	.detail{
+		font-size: 14px;
+	}
+</style>
