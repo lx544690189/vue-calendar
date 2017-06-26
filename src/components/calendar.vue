a171d66d8f9946445d072b1831df4fa020e9d158
lx544690189 <lx544690189@users.noreply.github.com>
2017-06-27 00:00:22 +0800

    新增demo用例

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,79 +1,81 @@
 <template>
-	<transition name="popup-slide-bottom">
-		<div class="calendar" v-show="open">
-			<div class="toolbar">
-				<div class="toolbar-inner">
-					<div class="calendar-month-picker">
-						<a href="#" class="calendar-prev" @click="handelMonthClick('pre')">
-							<div class="pre-img"></div>
-						</a>
-						<div class="current-month-value">{{ month[currentViewMonth.month - 1]}}</div>
-						<a href="#" class="calendar-next" @click="handelMonthClick('next')">
-							<div class="next-img"></div>
-						</a>
+	<div class="modal" :class="{show:open}" @click="handelModalClick">
+		<transition name="popup-slide-bottom">
+			<div class="calendar" v-show="open" ref="calendar">
+				<div class="toolbar">
+					<div class="toolbar-inner">
+						<div class="calendar-month-picker">
+							<a href="#" class="calendar-prev" @click="handelMonthClick('pre')">
+								<div class="pre-img"></div>
+							</a>
+							<div class="current-month-value">{{ month[currentViewMonth.month - 1]}}</div>
+							<a href="#" class="calendar-next" @click="handelMonthClick('next')">
+								<div class="next-img"></div>
+							</a>
+						</div>
+						<div class="calendar-year-picker">
+							<a href="#" class="calendar-prev" @click="handelYearClick('pre')">
+								<div class="pre-img"></div>
+							</a>
+							<div class="current-year-value">{{ currentViewMonth.year }}</div>
+							<a href="#" class="calendar-next" @click="handelYearClick('next')">
+								<div class="next-img"></div>
+							</a>
+						</div>
 					</div>
-					<div class="calendar-year-picker">
-						<a href="#" class="calendar-prev" @click="handelYearClick('pre')">
-							<div class="pre-img"></div>
-						</a>
-						<div class="current-year-value">{{ currentViewMonth.year }}</div>
-						<a href="#" class="calendar-next" @click="handelYearClick('next')">
-							<div class="next-img"></div>
-						</a>
+				</div>
+				<div class="calendar-inner">
+					<div class="calendar-week">
+						<div class="calendar-week-day" v-for="item in week">{{item}}</div>
+					</div>
+					<div class="calendar-month">
+						<div class="calendar-month-wrapper"
+							:style="{'transform': 'translate3d(' + (translateX_parent) + '%, 0, 0)'}">
+							<div class="calendar-month-pre"
+								:style="{'transform': 'translate3d(' + (translateX-100) + '%, 0, 0)'}">
+								<div class="calendar-month-row" v-for="(row, row_index) in dateObj.pre.allDay_list">
+									<div class="calendar-day"
+										v-for="(col, col_index) in row"
+										@click="handelDayClick(col,col_index,dateObj.pre.start,dateObj.pre.end)"
+										:class="{'grey':row_index*7+row_index*7+col_index<dateObj.pre.start || row_index*7+col_index>dateObj.pre.end ,'selected':col.selected }">
+										<span :class="isValid(dateObj.pre.start,dateObj.pre.end,row_index,col_index,col.value)">
+											{{ col.value }}
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class="calendar-month-current"
+								:style="{'transform': 'translate3d(' + translateX + '%, 0, 0)'}">
+								<div class="calendar-month-row" v-for="(row, row_index) in dateObj.current.allDay_list">
+									<div class="calendar-day"
+										v-for="(col, col_index) in row"
+										@click="handelDayClick(col,row_index*7+col_index,dateObj.current.start,dateObj.current.end)"
+										:class="{'grey':row_index*7+col_index<dateObj.current.start || row_index*7+col_index>dateObj.current.end ,'selected':col.selected }">
+										<span :class="isValid(dateObj.current.start,dateObj.current.end,row_index,col_index,col.value)">
+											{{ col.value }}
+										</span>
+									</div>
+								</div>
+							</div>
+							<div class="calendar-month-next"
+								:style="{'transform': 'translate3d(' + (translateX+100) + '%, 0, 0)'}">
+								<div class="calendar-month-row" v-for="(row, row_index) in dateObj.next.allDay_list">
+									<div class="calendar-day"
+										v-for="(col, col_index) in row"
+										@click="handelDayClick(col,row_index*7+col_index,dateObj.next.start,dateObj.next.end)"
+										:class="{'grey':row_index*7+col_index<dateObj.next.start || row_index*7+col_index>dateObj.next.end ,'selected':col.selected }">
+										<span :class="isValid(dateObj.next.start,dateObj.next.end,row_index,col_index,col.value)">
+											{{ col.value }}
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="calendar-inner">
-				<div class="calendar-week">
-					<div class="calendar-week-day" v-for="item in week">{{item}}</div>
-				</div>
-				<div class="calendar-month">
-					<div class="calendar-month-wrapper"
-						:style="{'transform': 'translate3d(' + (translateX_parent) + '%, 0, 0)'}">
-						<div class="calendar-month-pre"
-							:style="{'transform': 'translate3d(' + (translateX-100) + '%, 0, 0)'}">
-							<div class="calendar-month-row" v-for="(row, row_index) in dateObj.pre.allDay_list">
-								<div class="calendar-day"
-									v-for="(col, col_index) in row"
-									@click="handelDayClick(col,col_index,dateObj.pre.start,dateObj.pre.end)"
-									:class="{'grey':row_index*7+row_index*7+col_index<dateObj.pre.start || row_index*7+col_index>dateObj.pre.end ,'selected':col.selected }">
-									<span>
-										{{ col.value }}
-									</span>
-								</div>
-							</div>
-						</div>
-						<div class="calendar-month-current"
-							:style="{'transform': 'translate3d(' + translateX + '%, 0, 0)'}">
-							<div class="calendar-month-row" v-for="(row, row_index) in dateObj.current.allDay_list">
-								<div class="calendar-day"
-									v-for="(col, col_index) in row"
-									@click="handelDayClick(col,row_index*7+col_index,dateObj.current.start,dateObj.current.end)"
-									:class="{'grey':row_index*7+col_index<dateObj.current.start || row_index*7+col_index>dateObj.current.end ,'selected':col.selected }">
-									<span>
-										{{ col.value }}
-									</span>
-								</div>
-							</div>
-						</div>
-						<div class="calendar-month-next"
-							:style="{'transform': 'translate3d(' + (translateX+100) + '%, 0, 0)'}">
-							<div class="calendar-month-row" v-for="(row, row_index) in dateObj.next.allDay_list">
-								<div class="calendar-day"
-									v-for="(col, col_index) in row"
-									@click="handelDayClick(col,row_index*7+col_index,dateObj.next.start,dateObj.next.end)"
-									:class="{'grey':row_index*7+col_index<dateObj.next.start || row_index*7+col_index>dateObj.next.end ,'selected':col.selected }">
-									<span>
-										{{ col.value }}
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</transition>
+		</transition>
+	</div>
 </template>
 
 <script>
@@ -84,11 +86,23 @@
 				type: Boolean,
 				default: false
 			},
+			closeByClickmask: {
+				type: Boolean,
+				default: true
+			},
 			format: {
 				type: String,
 				default: "yyyy-MM-dd"
 			},
 			defaultDate: {
+				type: Date,
+        		default:null
+			},
+			maxDate:{
+				type: Date,
+        		default:null
+			},
+			minDate:{
 				type: Date,
         		default:null
 			},
@@ -176,6 +190,11 @@
 				}
 				this.createdDate();
 			},
+			handelModalClick(e){
+				if(this.closeByClickmask&&!this.$refs.calendar.contains(e.target)){
+					this.open = false;
+				}
+			},
 			//创建日期
 			createdDate() {
 				let year = this.currentViewMonth.year;
@@ -195,6 +214,33 @@
 				}
 				return true;
 			},
+			//判断日期是否可选,并标注当天
+			isValid(start,end,row_index,col_index,day){
+				if(!this.minDate&&!this.maxDate)return null;
+				let date,
+					year = this.currentViewMonth.year,
+					month = this.currentViewMonth.month - 1,
+					today = new Date(),
+					invalid = false,
+					isToday = false;
+				if(row_index*7 + col_index < start){
+					date = new Date(month==0?year-1:year,month==0?11:month-1,day);
+				}else if(row_index*7 + col_index > end){
+					date = new Date(month==11?year+1:year,month==11?1:month+1,day);
+				}else{
+					date = new Date(year,month,day);
+				}
+				if(0 <= today-date && today-date < 24*60*60*1000 ){
+					isToday = true;
+				}
+				if(this.minDate && date - new Date(this.minDate.getFullYear(),this.minDate.getMonth(),this.minDate.getUTCDate()+1) < 0){
+					invalid = true;
+				}
+				if(this.maxDate && date - new Date(this.maxDate.getFullYear(),this.maxDate.getMonth(),this.maxDate.getUTCDate()+1) > 0){
+					invalid = true;
+				}
+				return {'invalid':invalid,'today':isToday};
+			},
 			//月份切换
 			handelMonthClick(action,translateCheck){
 				if(translateCheck&&!this.isTranslateEnd())return;
@@ -210,7 +256,7 @@
 					setTimeout(()=>{
 						this.translateX_parent -= 100;
 						this.createdDate();
-					},430)
+					},450)
 				}
 				if(action == "next"){
 					this.translateX -= 100;
@@ -223,7 +269,7 @@
 					setTimeout(()=>{
 						this.translateX_parent += 100;
 						this.createdDate();
-					},430)
+					},450)
 				}
 			},
 			//年份切换
@@ -359,6 +405,21 @@
 </script>
 
 <style scoped>
+	.modal{
+		position: absolute;
+	    left: 0;
+	    top: 0;
+	    background: rgba(0,0,0,.4);
+	    z-index: 9999;
+	    opacity: 0;
+	    transition: opacity .3s;
+	    -webkit-transition: opacity .3s;
+	}
+	.modal.show{
+		width: 100%;
+	    height: 100%;
+		opacity: 1;
+	}
 	.calendar {
 		position: fixed;
 		bottom: 0;
@@ -368,7 +429,9 @@
 		background-color: #fff;
 		z-index: 10000;
 		transform: translateY(0);
+		-webkit-transform: translateY(0);
 		transition-duration: .2s;
+		-webkit-transition-duration: .2s;
 		transition-timing-function: linear;
 		-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
    		-webkit-touch-callout: none;
@@ -546,7 +609,9 @@
 		height: 1px;
 		background-color: #ccc;
 		transform: translateY(-0.5px);
+		-webkit-transform: translateY(-0.5px);
 		transform: scaleY(0.5);
+		-webkit-transform: scaleY(0.5);
 	}
 	
 	.calendar-day {
@@ -563,7 +628,7 @@
 	}
 	
 	.calendar-day.grey{
-		color: #ccc;
+		color: #757575;
 	}
 	
 	.calendar-day span{
@@ -574,6 +639,12 @@
 	    border-radius: 100%;
 	}
 	
+	.calendar-day span.invalid{
+		color: #e0e0e0;
+	}
+	.calendar-day span.today{
+		background-color: #eeeeee;
+	}
 	.calendar-day.selected span{
 		color: #fff;
 		background: #0894ec;
